30fd4450124035d4d71b7e4784c8c1e51b010475
Arjun Khurana <arjunkh99@gmail.com>
2020-10-23 12:41:03 -0500

    added math server file

---
+++ mathServer.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.net.*;
+import java.util.ArrayList;
+
+class mathServer
+{
+    private ArrayList<String> clients;
+    public static void main(String[] args) throws Exception
+    {
+        DatagramSocket serverSocket = new DatagramSocket(9876);
+
+    }
+}
